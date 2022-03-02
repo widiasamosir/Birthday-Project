45a030cb6b154e5ff380c8b5c5692b365ffde90b
widiasamosir <widiasamosir@gmail.com>
2022-03-02 20:41:24 +0700

    Changes on landing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,60 +10,14 @@ import { ActivatedRoute, Params } from '@angular/router';
 
 export class AppComponent {
 
-  colors : Array<string> = [
-
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red', 
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red', 
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-    'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
-
-  ];
-
-  who : string;
+  who : string = 'Andi Haryanto';
 
   colorSelected : string = 'red';
 
   source : Observable<number> = timer(1000, 500);
 
   constructor(private activatedRoute : ActivatedRoute) {
-    this.source.subscribe((val: number) => {
-         this.colorSelected = this.colors[val];                    
-  });
-   this.activatedRoute.queryParams.subscribe((params: Params) => {
 
-    // console.log(params);
-    
-    if(params['who']) {
-        let names : string[] = String(params['who']).split(' '); 
-        names.map((name, index, array) => {
-          array[index] = name.replace(name.slice(0, 1), name.charAt(0).toUpperCase())
-        });
-     // console.log('names: ', names.join(' '));
-        this.who = names.join(' ');
-    } else {
-      this.who = 'To You';
-    }
-  });
 
   }
 
